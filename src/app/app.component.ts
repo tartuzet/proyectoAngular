94c4a8b234264421a6483c8fb6bce0b8f84ac893
Gustavo Isidoro <tartuzet@gmail.com>
2017-03-14 18:44:04 -0600

    add output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,16 @@ import {InputComponent} from './input/input.component';
 })
 export class AppComponent {
   title = 'app works!';
+  votacion = '';
   votos=[
       {title: 'option 1 '},
-      {title: 'option 4 '},
+      {title: 'option 2 '},
       {title: 'option 3 '},
       {title: 'option 4 '}
   ];
+
+  addVoto(response:string){
+    this.votacion= "usted eligio: " +response;
+  }
 
 }
